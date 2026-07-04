40abf5abc1f37c4387fe3c908dba1395d4c6cfaf
908566141 <89303172+908566141@users.noreply.github.com>
2026-07-04 15:42:35 +0800

    fix: resolve mobile babel preset under isolated pnpm

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,10 +1,21 @@
+const { dirname } = require("pathe")
+
+const resolveFromExpo = (request) =>
+  require.resolve(request, { paths: [dirname(require.resolve("expo/package.json"))] })
+
 module.exports = function (api) {
   api.cache(true)
   return {
     presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind", unstable_transformImportMeta: true }],
-      "nativewind/babel",
+      [
+        resolveFromExpo("babel-preset-expo"),
+        { jsxImportSource: "nativewind", unstable_transformImportMeta: true },
+      ],
+      require.resolve("nativewind/babel"),
     ],
-    plugins: [["inline-import", { extensions: [".sql"] }], "react-native-worklets/plugin"],
+    plugins: [
+      [require.resolve("babel-plugin-inline-import"), { extensions: [".sql"] }],
+      require.resolve("react-native-worklets/plugin"),
+    ],
   }
 }
